486b522891ff945449ae7516e1fdf64d3d5a460b
Daniel Crenna <daniel.crenna@gmail.com>
2019-08-20 02:24:55 -0400

    nit: formatting pass

--- SideCar/Extensions/StringExtensions.cs
+++ SideCar/Extensions/StringExtensions.cs
@@ -1,4 +1,7 @@
-﻿namespace SideCar.Extensions
+﻿// Copyright (c) Daniel Crenna & Contributors. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace SideCar.Extensions
 {
 	public static class StringExtensions
 	{
