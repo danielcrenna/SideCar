3626b4c16bee1cb41f4403eb69ee9c2c13c3b07f
Daniel <daniel.crenna@gmail.com>
2019-10-23 14:40:57 -0400

    make client demo app work standalone

--- MyBlazorApp/ClientStartup.cs
+++ MyBlazorApp/ClientStartup.cs
@@ -7,7 +7,7 @@ namespace MyBlazorApp
     {
         public void ConfigureServices(IServiceCollection services)
         {
-	        /* Do not put anything in here, as it will likely result in a silently broken build */
+	        
         }
 
         public void Configure(IComponentsApplicationBuilder app)
